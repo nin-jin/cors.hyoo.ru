00a6142dca6c4f27a691b7712559911c5effcff7
nin-jin <sairi.na.tenshi@gmail.com>
2017-10-04 13:17:11 +0000

    error proof

--- server.js
+++ server.js
@@ -4,10 +4,18 @@ const httpProxy = require('http-proxy')
 var proxy = httpProxy.createProxyServer({ changeOrigin : true , ignorePath : true })
 
 var server = http.createServer(function(req, res) {
-  res.setHeader( 'Access-Control-Allow-Origin' , req.headers['Origin'] || '*' )
-  const target = req.url.substring(1)
-  console.log( target )
-  proxy.web(req, res, { ignorePath : true , target })
+  const domain = require('domain').create()
+  domain.on('error', error => {
+    console.error(error.stack)
+    res.statusCode = 500
+    res.end( error.message )
+  })
+  domain.run(() => {
+    res.setHeader( 'Access-Control-Allow-Origin' , req.headers['Origin'] || '*' )
+    const target = req.url.substring(1)
+    console.log( target )
+    proxy.web(req, res, { ignorePath : true , target })
+  })
 });
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
